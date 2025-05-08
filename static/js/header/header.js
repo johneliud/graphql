5b220d472a5c9a85f1d38fc8a3a0b22b1789f67b
johneliud <johneliud4@gmail.com>
2025-05-08 11:37:13 +0300

    fix: standardize quotes and add size attributes to theme toggler icons

--- static/js/header/header.js
+++ static/js/header/header.js
@@ -1,17 +1,17 @@
 export async function renderHeader() {
-  const app = document.getElementById('app');
-  const header = document.createElement('header');
-  header.className = 'header';
-  header.id = 'header';
+  const app = document.getElementById("app");
+  const header = document.createElement("header");
+  header.className = "header";
+  header.id = "header";
 
   header.innerHTML = `
     <div class="logo">
       <a href="#">GraphQL</a>
     </div>
 
-    <div class="theme-toggler">
-      <box-icon name='sun' class="sun"></box-icon>
-      <box-icon name='moon' class="moon"></box-icon>
+    <div id="themeToggler" class="theme-toggler">
+      <box-icon name='sun' size="md" class="sun"></box-icon>
+      <box-icon name='moon' size="md" class="moon"></box-icon>
     </div>
   `;
   app.appendChild(header);

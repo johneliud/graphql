a6bc3d8498820445d3fb9a87e0e76ebadb77359a
johneliud <johneliud4@gmail.com>
2025-05-08 11:38:40 +0300

    add: implement header rendering and theme toggler functionality in script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+import { renderHeader } from "./header/header.js";
+import { toggleTheme } from "./toggle_theme.js";
+
+renderHeader();
+
+// Theme functionality
+const themeToggler = document.getElementById("themeToggler");
+themeToggler.addEventListener("click", toggleTheme);
+
+toggleTheme();

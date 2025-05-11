e70fa197095f87e7bf350ecedfa1888e59865ff3
johneliud <johneliud4@gmail.com>
2025-05-11 12:09:28 +0300

    fix: remove unused imports and redundant function calls in script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,6 @@
+import './profile.js';
 import { renderHeader } from './header/header.js';
 import { toggleTheme } from './toggle_theme.js';
-import { renderSignInView } from './signin/signin_view.js';
-import { validateSignInFormData } from './signin/signin_validation.js';
 
 renderHeader();
 
@@ -9,8 +8,4 @@ renderHeader();
 const themeToggler = document.getElementById('themeToggler');
 themeToggler.addEventListener('click', toggleTheme);
 
-toggleTheme();
-
 renderSignInView();
-
-validateSignInFormData();

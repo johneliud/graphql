5740fa002fce332a3f0f12b4c99bc1c7f9c70798
johneliud <johneliud4@gmail.com>
2025-05-11 12:39:06 +0300

    fix: remove unused profile import and ensure signin view initialization

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,15 @@
-import './profile.js';
 import { renderHeader } from './header/header.js';
 import { toggleTheme } from './toggle_theme.js';
+import { renderSignInView } from './signin/signin_view.js';
+import { validateSignInFormData } from './signin/signin_validation.js';
 
+// Initialize header
 renderHeader();
 
 // Theme functionality
 const themeToggler = document.getElementById('themeToggler');
 themeToggler.addEventListener('click', toggleTheme);
 
+// Initialize signin view
 renderSignInView();
+validateSignInFormData();

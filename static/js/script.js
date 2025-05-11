747854ec0c513cc687d02eab6af812d462dee8a9
johneliud <johneliud4@gmail.com>
2025-05-11 10:42:48 +0300

    style: standardize quotes and formatting in script.js for consistency

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,16 @@
-import { renderHeader } from "./header/header.js";
-import { toggleTheme } from "./toggle_theme.js";
-import { renderSignInView } from "./signin/signin_view.js";
+import { renderHeader } from './header/header.js';
+import { toggleTheme } from './toggle_theme.js';
+import { renderSignInView } from './signin/signin_view.js';
+import { validateSignInFormData } from './signin/signin_validation.js';
 
 renderHeader();
 
 // Theme functionality
-const themeToggler = document.getElementById("themeToggler");
-themeToggler.addEventListener("click", toggleTheme);
+const themeToggler = document.getElementById('themeToggler');
+themeToggler.addEventListener('click', toggleTheme);
 
 toggleTheme();
 
 renderSignInView();
+
+validateSignInFormData();

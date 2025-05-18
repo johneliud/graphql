213ba87c038cdc5c3573452b92ac0fd57889abb6
johneliud <johneliud4@gmail.com>
2025-05-18 17:23:35 +0300

    fix: remove logout button and related logic from header rendering

--- static/js/header/header.js
+++ static/js/header/header.js
@@ -1,12 +1,8 @@
-import { logout } from '../validate_jwt.js';
-
 export async function renderHeader() {
   const app = document.getElementById('app');
   const header = document.createElement('header');
   header.className = 'header';
   header.id = 'header';
-
-  const isLoggedIn = localStorage.getItem('jwt') ? true : false;
 
   header.innerHTML = `
     <div class="logo">
@@ -14,11 +10,6 @@ export async function renderHeader() {
     </div>
 
     <div class="header-controls">
-      ${
-        isLoggedIn
-          ? '<button id="logoutBtn" class="logout-btn">Log Out</button>'
-          : ''
-      }
       <div id="themeToggler" class="theme-toggler">
         <box-icon name='sun' size="md" class="sun"></box-icon>
         <box-icon name='moon' size="md" class="moon"></box-icon>
@@ -32,8 +23,4 @@ export async function renderHeader() {
   document.getElementById('homeLink').addEventListener('click', (e) => {
     e.preventDefault();
   });
-
-  if (isLoggedIn) {
-    document.getElementById('logoutBtn').addEventListener('click', logout);
-  }
 }
